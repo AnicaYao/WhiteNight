8ed1a356e4b22eb212472213c7c96f0c784fbce4
Oliver Steele <steele@osteele.com>
2020-05-15 16:56:55 -0400

    Rename example -> public

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("example"));
+app.use(express.static("public"));
 
 // check the config file
 const config = require("./config.json");
